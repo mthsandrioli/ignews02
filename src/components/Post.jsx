f9825e2a80e72c8f6844feec5ec6779f3f37bc03
mthsandrioli <matheus_andrioli@hotmail.com>
2025-02-05 10:02:26 -0300

    feat: form validation

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,43 +1,53 @@
 import { format, formatDistanceToNow } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
+import { useState } from 'react';
 
-import { Avatar } from "./Avatar";
-import { Comment } from "./Comment";
+import { Avatar } from './Avatar';
+import { Comment } from './Comment';
 
-import styles from "./Post.module.css";
-import { useCallback, useState } from 'react';
+import styles from './Post.module.css';
 
 export function Post({ author, publishedAt, content }) {
   const [comments, setComments] = useState([
     'Post muito bacana, hein?!'
-  ])
-  const [newCommentText, setNewCommentText] = useState('')
-  
+  ]);
+
+  const [newCommentText, setNewCommentText] = useState('');
+
   const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
     locale: ptBR,
   });
+
   const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
     locale: ptBR,
     addSuffix: true
   });
 
-  function handleCreateNewComment() {
+  function handleCrateNewComment() {
     event.preventDefault()
-    
-    setComments([...comments, newCommentText])
-    setNewCommentText('')
+
+    setComments([...comments, newCommentText]);
+    setNewCommentText('');
   }
 
   function handleNewCommentChange() {
-    setNewCommentText(event.target.value)
+    event.target.setCustomValidity('');
+    setNewCommentText(event.target.value);
+  }
+
+  function handleNewCommentInvalid() {
+    event.target.setCustomValidity('Esse campo é obrigatório!');
   }
 
   function deleteComment(commentToDelete) {
     const commentsWithoutDeletedOne = comments.filter(comment => {
       return comment !== commentToDelete;
     })
+
     setComments(commentsWithoutDeletedOne);
   }
+
+  const isNewCommentEmpty = newCommentText.length === 0;
 
   return (
     <article className={styles.post}>
@@ -56,34 +66,39 @@ export function Post({ author, publishedAt, content }) {
       </header>
 
       <div className={styles.content}>
-        {content.map((line, index) => {
+        {content.map(line => {
           if (line.type === 'paragraph') {
-            return <p key={index} >{line.content}</p>;
+            return <p key={line.content}>{line.content}</p>;
           } else if (line.type === 'link') {
-            return <p key={index}><a href="#">{line.content}</a></p>
+            return <p key={line.content}><a href="#">{line.content}</a></p>
           }
         })}
       </div>
 
-      <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
+      <form onSubmit={handleCrateNewComment} className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
 
-        <textarea 
-          placeholder="Deixe um comentário" 
+        <textarea
+          name="comment"
+          placeholder="Deixe um comentário"
           value={newCommentText}
-          onChange={handleNewCommentChange} 
+          onChange={handleNewCommentChange}
+          onInvalid={handleNewCommentInvalid}
+          required
         />
 
         <footer>
-          <button type="submit">Publicar</button>
+          <button type="submit" disabled={isNewCommentEmpty}>
+            Publicar
+          </button>
         </footer>
       </form>
 
       <div className={styles.commentList}>
-        {comments.map((comment, index) => {
+        {comments.map(comment => {
           return (
             <Comment
-              key={index}
+              key={comment}
               content={comment}
               onDeleteComment={deleteComment}
             />
@@ -91,5 +106,5 @@ export function Post({ author, publishedAt, content }) {
         })}
       </div>
     </article>
-  );
+  )
 }
